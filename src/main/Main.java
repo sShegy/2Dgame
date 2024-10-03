00ff0ee8bd9595e24bb191a82bce2b4a06de1d41
Shegy <msegan13323ri@raf.rs>
2024-10-03 23:11:37 +0200

    Added Logo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,20 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JFrame window=new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D adveture");
+
+        Image Logo = ImageIO.read(new File("C:\\Users\\Shegy\\Desktop\\2Dgame\\res\\player\\Logo.jpg"));
+        window.setIconImage(Logo);
 
         GameBanner gameBanner=new GameBanner();
         window.add(gameBanner);
